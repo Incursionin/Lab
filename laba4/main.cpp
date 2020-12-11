f28a2c445b258e3bb5309c32a31dbc1cafa133fa
Maxim_Subbotkin <maks.subbotkin@bk.ru>
2020-12-11 10:15:08 +0300

    Добвалены списки и тд

--- laba4/main.cpp
+++ laba4/main.cpp
@@ -5,6 +5,95 @@
 #include <stack>
 #include <queue>
 using namespace std;
+typedef struct NODE {
+	int v;
+	NODE* Next;
+};
+typedef struct LIST {
+	int M;
+	struct NODE** HEAD;
+};
+NODE* CreateNODE(int v) {
+	NODE* NewNODE = (NODE*)malloc(sizeof(NODE));
+	NewNODE->v = v;
+	NewNODE->Next = NULL;
+	return NewNODE;
+}
+LIST* CreateLIST(int M) {
+
+	LIST* list = (LIST*)malloc(sizeof(LIST));
+	list->M = M;
+	list->HEAD = (NODE**)malloc(sizeof(struct NODE*) * M);
+	for (int i = 0; i < M; i++) {
+		list->HEAD[i] = NULL;
+	}
+	return list;
+}
+void AddFirstElement(NODE** HEAD, NODE* NewNODE) {
+	NewNODE->Next = *HEAD;
+	*HEAD = NewNODE;
+}
+void after(NODE* AfterNODE, NODE* NewNODE) {
+	NewNODE->Next = AfterNODE->Next;
+	AfterNODE->Next = NewNODE;
+}
+void before(NODE** HEAD, NODE* BeforeNODE, NODE* NewNODE) {
+	NODE* active = *HEAD;
+	if (*HEAD == BeforeNODE) {
+		AddFirstElement(HEAD, NewNODE);
+		return;
+	}
+	while (active && active->Next != BeforeNODE)
+		active = active->Next;
+	if (active)
+		after(active, NewNODE);
+}
+void DFSLIST(int v, LIST* list, int* visitedArray, int N) {
+	printf("%d ", v + 1);
+
+	visitedArray[v] = 1;
+	NODE* active = list->HEAD[v];
+	while (active) {
+		if (visitedArray[active->v] == 0) {
+			DFSLIST(active->v, list, visitedArray, N);
+		}
+		active = active->Next;
+	}
+}
+
+
+void AddLastElement(NODE** HEAD, NODE* NewNODE) {
+	NODE* active = *HEAD;
+	if (*HEAD == NULL) {
+		AddFirstElement(HEAD, NewNODE);
+		return;
+	}
+	while (active->Next)
+		active = active->Next;
+	after(active, NewNODE);
+}
+void outputLIST(LIST* list) {
+	NODE* active;
+	for (int i = 0; i < list->M; i++) {
+		printf("%d", i + 1);
+		active = list->HEAD[i];
+		while (active != NULL) {
+			printf("->%d", active->v + 1);
+			active = active->Next;
+		}
+		printf("\n");
+	}
+}
+void transfer(int** Array, int N, LIST* list) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (Array[i][j] == 1) {
+				NODE* NewNODE = CreateNODE(j);
+				AddLastElement(&list->HEAD[i], NewNODE);
+			}
+		}
+	}
+}
 
 void BFS(int vertex, int** mass, int* vis, int f) {
 	queue<int>BFS;
@@ -77,6 +166,18 @@ int main() {
 		if (vis[vert] == 0) {
 			printf("\n");
 			BFS(vert, mass, vis, f);
+		}
+	}
+	LIST* list = CreateLIST(f);
+	transfer(mass, f, list);
+	printf("\n");
+	outputLIST(list);
+	for (int i = 0; i < f; i++) {
+		vis[i] = 0;
+	}
+	for (int i = 0; i < f; i++) {
+		if (vis[i] == 0) {
+			DFSLIST(i, list, vis, f);
 		}
 	}
 
